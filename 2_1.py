0939e4b47ca2b7e29ca8c7b7f794ed3c2c778a03
spartame <942726830@qq.com>
2018-01-25 22:59:25 +0800

    Chapter 2 Practice

---
+++ 2_1.py
@@ -0,0 +1,16 @@
+#2-1
+say = 'Hello world!'
+print(say)
+
+#2-2
+say = 'hey'
+print(say)
+
+#2-3
+name = "jane"
+print("Hello " + name + ", how are you?")
+
+#2-4
+print(name.title())
+print(name.upper())
+print(name.lower())
